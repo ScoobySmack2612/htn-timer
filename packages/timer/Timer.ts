bec34937904dd1c7bc5734401304a1e5b1b5a181
Heron Navarro <hnavarro@cbtnuggets.com>
2020-12-11 22:33:46 -0800

    timer logic & tests

--- packages/timer/Timer.ts
+++ packages/timer/Timer.ts
@@ -1,7 +1,52 @@
-export class Timer {
-    duration;
+import moment, { Moment } from 'moment';
 
-    constructor() {
-        this.duration = 0;
+type TimerConfiguration = {
+    expires?: Moment;
+    milliseconds?: number
+    seconds?: number
+    hours?: number
+}
+export class Timer {
+    expires: Moment;
+    seconds: number;
+    milliseconds: number;
+    hours: number;
+
+    constructor(configuration?: TimerConfiguration) {
+        if (typeof configuration === 'object') {
+            const { expires, milliseconds, seconds, hours } = configuration;
+            this.expires = expires;
+            this.milliseconds = milliseconds;
+            this.seconds = seconds;
+            this.hours = hours;
+        }
+
+        this.expires = null;
+        this.milliseconds = 0;
+        this.seconds = 0;
+        this.hours = 0;
     }
+
+    setMilliseconds(milliseconds: number) {
+        this.milliseconds = milliseconds;
+    }
+
+    setSeconds(seconds: number) {
+        this.seconds = seconds;
+    }
+    setHours(hours: number) {
+        this.hours = hours;
+    }
+
+    setExpiration(startedAt: Moment) {
+        const millisInHours = Math.floor(this.hours * 60 * 60 * 1000);
+        const millisInSeconds = Math.floor(this.seconds * 1000);
+
+        this.expires = moment(startedAt).add(millisInHours + millisInSeconds + this.milliseconds, 'milliseconds')
+    }
+
+    start(startedAt: Moment = moment()) {
+        this.setExpiration(startedAt)
+    }
+
 }
--- packages/timer/__test__/timer.test.ts
+++ packages/timer/__test__/timer.test.ts
@@ -1,9 +1,34 @@
 import { Timer } from '../Timer';
+import moment = require('moment');
 
 describe('Timer', () => {
-    it('Timer initializes to 0', () => {
+    it('Timer initializes', () => {
         const timer = new Timer();
-        expect(timer).toHaveProperty('duration');
-        expect(timer.duration).toEqual(0);
+        expect(timer).toHaveProperty('expires');
+        expect(timer.expires).toEqual(null);
+    })
+    it('accepts milliseconds', () => {
+        const timer = new Timer();
+        const startTime = moment();
+        timer.setMilliseconds(1)
+        timer.start(startTime)
+
+        expect(timer.expires.diff(startTime, 'milliseconds')).toEqual(1);
+    })
+    it('accepts seconds', () => {
+        const timer = new Timer();
+        const startTime = moment();
+        timer.setSeconds(1)
+        timer.start(startTime)
+
+        expect(timer.expires.diff(startTime, 'second')).toEqual(1);
+    })
+    it('accepts hours', () => {
+        const timer = new Timer();
+        const startTime = moment();
+        timer.setHours(1)
+        timer.start(startTime)
+
+        expect(timer.expires.diff(startTime, 'hours')).toEqual(1);
     })
 })
